176ca54fa41d544d907eeb0a6a284ae83c9232f2
Jabez <jabezdailey@icloud.com>
2025-06-04 19:04:41 -0400

    improved gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -25,9 +25,15 @@ const Gallery = () => {
       <main className="flex-grow w-full">
         <section className="bg-primary px-2 mx-auto flex flex-col gap-2 py-6 md:px-6 lg:px-12 xl:px-16 2xl:px-24">
           <h1 className="font-heading text-3xl pb-3 text-white text-center">Our Gallery</h1>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
+          <div className="columns-2 md:columns-3 lg:columns-4 gap-3 space-y-3">
             {imageFilenames.map((name, i) => (
-              <img key={i} src={`/gallery/${name}`} alt={`Image ${i}`} style={{width: '100%'}} />
+              <img
+                key={i}
+                src={`/gallery/${name}`}
+                alt={`Image ${i}`}
+                className="w-full mb-3 rounded"
+                style={{ display: 'block' }}
+              />
             ))}
           </div>
         </section>
